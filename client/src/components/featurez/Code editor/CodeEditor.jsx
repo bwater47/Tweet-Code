cc85072bd6c52f31a5d2393907918015fe3fb775
B Water <brennw47@gmail.com>
2024-07-31 15:09:35 -0500

    Added headlessUI to the client package.json. Added the tailwind.config.js and postcss.config.js. Added the package.json to the server folder. Added a working LanguageSelector.

--- client/src/components/featurez/Code editor/CodeEditor.jsx
+++ client/src/components/featurez/Code editor/CodeEditor.jsx
@@ -1,31 +1,29 @@
 import { Editor } from "@monaco-editor/react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import LanguageSelector from "./LanguageSelector";
-
 export default function CodeEditor() {
   const editorRef = useRef()
   const [value, setValue] = useState("");
   const [language, setLanguage] = useState("javascript");
-
   const onMount = (editor) => {
     editorRef.current = editor;
     editor.focus();
   };
-
-  const onSelect = (language) => {
-    setLanguage(language);
+  const onSelect = (lang) => {
+    setLanguage(lang);
   }
-
   return (
-    <LanguageSelector language={language} onSelect={onSelect}/>
-    <Editor
-      height="75vh"
-      theme="vs-dark"
-      defaultLanguage="javascript"
-      defaultValue="// Write your code here"
-      onMount={onMount}
-      value={value}
-      onChange={(value) => setValue(value)}
-    />
+    <>
+      <LanguageSelector language={language} onSelect={onSelect}/>
+      <Editor
+        height="75vh"
+        theme="vs-dark"
+        language={language}
+        defaultValue="// Write your code here"
+        onMount={onMount}
+        value={value}
+        onChange={(value) => setValue(value)}
+      />
+    </>
   );
 }
