15394c0dcac11a1a1aebaf17f98b0213ee300bde
JacobToton <Jake_Toton@live.com>
2024-08-01 16:42:10 -0600

    Converting .js files to ESM
    -Removing require() statements and changing to imports
    - Adding .js extensions to appropriate file imports
    - Adjusting any module.exports with a named export
    
    Files Adjusted:
    - server.js
    - auth.js
    - index.js (schemas)
    - user.js
    - product.js
    - order.js
    - index.js (modules)
    - connection.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,5 +1,8 @@
-const mongoose = require("mongoose");
+// Import for ESM.
+import mongoose from "mongoose";
+
 const { Schema } = mongoose;
+
 const productSchema = new Schema({
   name: {
     type: String,
@@ -15,5 +18,7 @@ const productSchema = new Schema({
     min: 0.99,
   },
 });
+
 const Product = mongoose.model("Product", productSchema);
-module.exports = Product;
+
+export default Product;
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
-const User = require("./User");
-const Product = require("./Product");
-const Order = require("./Order");
+// Imports for ESM.
+import User from "./User.js";
+import Product from "./Product.js";
+import Order from "./Order.js";
 
-module.exports = { User, Product, Order };
+export { User, Product, Order };
