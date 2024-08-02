73306a7979434a0b7b4afc20424c49d714e59a14
B Water <brennw47@gmail.com>
2024-08-01 22:38:52 -0500

    feat(hook): add useDonationButton.js hook  Added `useDonationButton.js` hook for handling donation button logic.  refactor(footer): remove functionality, include boilerplate footer  Removed previous functionality from `footer.jsx` and replaced it with a boilerplate footer.  chore(main): add strict mode to React application  Enabled strict mode in the React application within `main.jsx`.  fix(app): correct page rendering issues  Fixed rendering issues in `app.jsx` to ensure proper page rendering.  fix(graphql): resolve undefined error in typeDefs.js  Updated `typeDefs.js` to fix undefined errors related to GraphQL schema definitions.
    feat(hook): add useDonationButton.js hook
    
    Added `useDonationButton.js` hook for handling donation button logic.
    
    refactor(footer): remove functionality, include boilerplate footer
    
    Removed previous functionality from `footer.jsx` and replaced it with a boilerplate footer.
    
    chore(main): add strict mode to React application
    
    Enabled strict mode in the React application within `main.jsx`.
    
    fix(app): correct page rendering issues
    
    Fixed rendering issues in `app.jsx` to ensure proper page rendering.
    
    fix(graphql): resolve undefined error in typeDefs.js
    
    Updated `typeDefs.js` to fix undefined errors related to GraphQL schema definitions.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+import { Outlet } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
-  
-
   return (
     <>
-      
-        <Header />
-          <Outlet/>
-        <Footer />
-      
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
