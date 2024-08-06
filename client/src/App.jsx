155accdaee85e85501425701bf63a320d9dd9012
JacobToton <Jake_Toton@live.com>
2024-08-05 18:24:51 -0600

    Removed comment / reminder for myself that was in the commit
    Removed comment / reminder for myself that was in the commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { AuthProvider } from "./hooks/useAuth.jsx"; // Make sure this path is correct
+import { AuthProvider } from "./hooks/useAuth.jsx";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import "./styles/App.css";
