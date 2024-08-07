25e72ca8292b906e4061a30d6be5c767ba5287f1
Ownerman123 <charlesshumway1@gmail.com>
2024-08-06 22:51:38 -0600

    add auto ad changes every 30 secs

--- client/src/components/common/adSpace.jsx
+++ client/src/components/common/adSpace.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import adImage from "../../assets/ads/ad-Dino.webp";
 import {log} from "../../utils/adswitcher"
 
+
 const AdSpace = () => {
+  const [image ,setimage] = useState(adImage);
+
+  useEffect(() => {const imgchange = setInterval(() => {
+    setimage(log());
+    console.log(image);
+  }, 30000 // set to 30 secs
+    ); 
+
+    return () => clearInterval(imgchange);
+});
+  
+ 
+  
+  
+  
   return (
     <>
     <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
       {/*Add Future Random Ad Selection Here*/}
-      <Image src={adImage} alt="Advertisement" />
+      <Image src={image} alt="Advertisement" />
     </Box>
     
     </>
--- client/src/utils/adswitcher.js
+++ client/src/utils/adswitcher.js
@@ -1,8 +1,13 @@
-import {Crunchy} from '../assets/ads/index.js'
+import {Crunchy,CupHub,Dino,EcoGadget,EcoGadget2,FreshFizz,Hair,HomeDefense,HydroBeam,Medication,SkinCare} from '../assets/ads/index.js'
 
+const images =[Crunchy,CupHub,Dino,EcoGadget,EcoGadget2,FreshFizz,Hair,HomeDefense,HydroBeam,Medication,SkinCare]
 function logimages() {
     
-    console.log(Crunchy);
+    const selected = images[Math.floor(Math.random() * images.length)]
+    console.log(Math.floor(Math.random() * images.length))
+
+
+    return selected;
 }
 
 export const log = logimages;
