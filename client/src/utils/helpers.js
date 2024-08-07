d71c48bc0ea60b3af5c39768513a888d102f36eb
Ownerman123 <charlesshumway1@gmail.com>
2024-08-07 10:20:43 -0600

    fix http link

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -16,7 +16,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 });
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3001/graphql', // Ensure this is the correct URL
+  uri: '/graphql', // Ensure this is the correct URL
   credentials: 'include', // Include credentials (cookies) in requests
 });
 
