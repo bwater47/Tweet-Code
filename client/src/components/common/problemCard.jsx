88794bfeaed37483db44e924558899886ea0ff1f
JacobToton <Jake_Toton@live.com>
2024-08-08 17:55:11 -0600

    New Query Single Problem / Updated Home.jsx
    - Added new query to queries.js ( GET_PROBLEM ) to locate a single problem
    - Updated home.jsx to include link to single problem in title for displayed problem
    - Link directs to /problem/_id
    - Example: http://localhost:3000/problem/66b2a5cfb105816b9f4ae0ea
    - Updated icons to be based off "programmingLanguage" to fix error in display

--- client/src/components/common/problemCard.jsx
+++ client/src/components/common/problemCard.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { Box, Heading, Text, Tag, HStack, Icon } from "@chakra-ui/react";
-import { FaJs, FaPython, FaJava, FaCode } from "react-icons/fa"; // We can import more icons as needed.
+import { Box, Heading, Text, Tag, HStack, Icon, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { FaJs, FaPython, FaJava, FaCode } from "react-icons/fa";
 
 const languageIcons = {
   JavaScript: FaJs,
   Python: FaPython,
   Java: FaJava,
-  // We can add more mappings as needed.
 };
 
 const ProblemCard = ({ problem }) => {
-  const LanguageIcon = languageIcons[problem.primaryLanguage] || FaCode;
+  const LanguageIcon = languageIcons[problem.programmingLanguage] || FaCode;
 
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4} bg='palette.darkgrey' borderColor='palette.grey' boxShadow='2px 0px 5px 2px '>
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={4}
+      mb={4}
+      bg="palette.darkgrey"
+      borderColor="palette.grey"
+      boxShadow="2px 0px 5px 2px "
+    >
       <HStack spacing={2} mb={2}>
-        <Icon as={LanguageIcon} w={6} h={6} color='palette.white' />
-        <Heading size="md" color='palette.white'>{problem.title}</Heading>
+        <Icon as={LanguageIcon} w={6} h={6} color="palette.white" />
+        <Link as={RouterLink} to={`/problem/${problem._id}`}>
+          <Heading size="md" color="palette.white">
+            {problem.title}
+          </Heading>
+        </Link>
       </HStack>
-      <Text noOfLines={2} mb={2} color='palette.white'>
+      <Text noOfLines={2} mb={2} color="palette.white">
         {problem.description}
       </Text>
       <HStack spacing={2}>
