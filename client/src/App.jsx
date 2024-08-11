4e8aaa10a061b9c2ddbb197a6033ba2b2837fe42
B Water <brennw47@gmail.com>
2024-08-10 21:08:30 -0500

    chore(comments): add comments to various files and remove Search.jsx  Added comments to the following files: - App.jsx - CodeEditor.jsx - useDonationButton.jsx - main.jsx - vite.config.js - Donation.js - DonationTransaction.js - index.js in models  Removed Search.jsx from main.jsx and the pages directory.
    chore(comments): add comments to various files and remove Search.jsx
    
    Added comments to the following files:
    - App.jsx
    - CodeEditor.jsx
    - useDonationButton.jsx
    - main.jsx
    - vite.config.js
    - Donation.js
    - DonationTransaction.js
    - index.js in models
    
    Removed Search.jsx from main.jsx and the pages directory.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
+// Import Outlet from react-router-dom to render child routes.
 import { Outlet } from "react-router-dom";
+// Import AuthProvider from useAuth.jsx.
 import { AuthProvider } from "./hooks/useAuth.jsx";
+// Import Header and Footer components.
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+// Import CSS file.
 import "./styles/App.css";
+// Import Chakra UI Box component.
 import { Box } from "@chakra-ui/react";
-
+// Creates a function that returns the layout of the app.
 function App() {
   return (
     <AuthProvider>
-    <>
-      <Header />
-      <Outlet />
-      <Box id="filler" bg="palette.darkgrey"></Box>
-      <Footer />
-    </>
+      <>
+        <Header />
+        <Outlet />
+        <Box id="filler" bg="palette.darkgrey"></Box>
+        <Footer />
+      </>
     </AuthProvider>
   );
 }
-
+// Export the App function.
 export default App;
--- server/models/Donation.js
+++ server/models/Donation.js
@@ -1,5 +1,8 @@
+// Import mongoose package.
 import mongoose from "mongoose";
+// Create a schema for donation.
 const { Schema } = mongoose;
+// Define the schema for donation.
 const donationSchema = new Schema({
   name: {
     type: String,
@@ -13,8 +16,10 @@ const donationSchema = new Schema({
     type: Number,
     required: true,
     min: 0.99,
-    default: 10.00,
+    default: 10.0,
   },
 });
+// Create a model using the schema, and pass the model name and schema as arguments.
 const Donation = mongoose.model("Donation", donationSchema);
+// Export the model.
 export default Donation;
