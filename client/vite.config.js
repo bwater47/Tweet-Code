716dd27a18f2ccc06dbf9f08f3402f019e4aef81
Ownerman123 <charlesshumway1@gmail.com>
2024-08-13 01:50:20 -0600

    tryn it" git push
    ""

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
 // https://vitejs.dev/config/
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const uri =
+  `https://ithinkididit--tweettweetcode.netlify.app/graphql`;
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
+    target: uri,
     // Add the proxy configuration to forward requests to the server running on port 3001.
     proxy: {
       "/graphql": {
