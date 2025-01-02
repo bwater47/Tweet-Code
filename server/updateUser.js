6840387e12848679e452c571acb495b17e48f956
B Water <brennw47@gmail.com>
2025-01-02 15:11:53 -0600

    docs(updateUser): Added a comment describing the dotenv.config functionality to updateUser.js.
    docs(updateUser): Added a comment describing the dotenv.config functionality to updateUser.js.

--- server/updateUser.js
+++ server/updateUser.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 
+// Loads environment variables from a .env file into process.env.
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
